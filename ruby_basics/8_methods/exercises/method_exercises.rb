3bcb2a189cf835d355fcce3dc10ab2d9adde8ea6
Israel Ogunmola <israelogunmola@gmail.com>
2023-08-17 17:51:00 +0100

    Solve method exercises

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,11 +7,18 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(item)
+    item.chr
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+
+def common_sports(current_sports, favorite_sports)
+    current_sports.intersection(favorite_sports)
+end
 
 
 # method name: #alphabetical_list
@@ -19,17 +26,28 @@
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
 
+def alphabetical_list(games)
+    games.sort.uniq
+end
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
 
+def lucky_number(num = 7)
+    "Today's lucky number is #{num}" 
+end
 
 # method name: #ascii_code
 # parameter: character (a string)
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
+
+def ascii_code(character)
+    return "Input Error" unless character.length == 1
+    character.ord
+end
 
 
 # method name: #pet_pun
@@ -40,9 +58,21 @@
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
 
+def pet_pun(animal)
+    puts case animal
+    when "cat" then "Cats are purr-fect!"
+    when "dog" then "Dogs are paw-some!"
+    else "I think #{animal}s have pet-tential!"
+    end
+end
+
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
+
+def twenty_first_century?(year)
+    year.between?(2001, 2100)
+end
 
